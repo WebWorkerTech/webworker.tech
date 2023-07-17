bf62052d07387592f066bad041b1ec43288608e5
Otto <fa@ijust.cc>
2023-07-17 16:00:39 +0800

    feat: 补充分享朋友圈的设置

--- blog/.vuepress/client.js
+++ blog/.vuepress/client.js
@@ -69,7 +69,7 @@ export default defineClientConfig({
       "/sdk/jweixin-1.6.0.js",
       // on script tag loaded.
       async (el) => {
-        const url = location.origin + location.pathname;
+        const url = location.href.split("#")[0];
         const res = await fetchSign(url);
         console.log("res", res);
 
@@ -80,11 +80,23 @@ export default defineClientConfig({
             timestamp: res.data.timestamp, // 必填，生成签名的时间戳
             nonceStr: res.data.nonceStr, // 必填，生成签名的随机串
             signature: res.data.signature, // 必填，签名
-            jsApiList: ["updateAppMessageShareData"], // 必填，需要使用的JS接口列表
+            jsApiList: ["updateAppMessageShareData", "updateTimelineShareData"], // 必填，需要使用的JS接口列表
           });
           wx.ready(function () {
             // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
-            console.log("ready");
+            console.log("wx.ready ready");
+            wx.checkJsApi({
+              jsApiList: [
+                "updateAppMessageShareData",
+                "updateTimelineShareData",
+              ], // 需要检测的JS接口列表，所有JS接口列表见附录2,
+              success: function (res) {
+                // 以键值对的形式返回，可用的api值true，不可用为false
+                // 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
+                console.log("checkJsApi res", res);
+              },
+            });
+
             wx.updateAppMessageShareData({
               title: "自定义标题", // 分享标题
               desc: "自定义描述", // 分享描述
@@ -93,6 +105,20 @@ export default defineClientConfig({
               success: function () {
                 // 设置成功
                 console.log("注册");
+              },
+              fail: function (err) {
+                console.log("注册失败", err);
+              },
+            });
+
+            wx.updateTimelineShareData({
+              title: "自定义标题2", // 分享标题
+              link: location.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+              imgUrl: "/img/logo-650.png", // 分享图标
+              success: function () {
+                // 设置成功
+                // 设置成功
+                console.log("设置朋友圈成功");
               },
             });
           });
