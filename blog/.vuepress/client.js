c832071ad8536ed57809cc94e075d48637be4018
Otto <fa@ijust.cc>
2023-07-17 15:44:01 +0800

    feat: 补充接口声明

--- blog/.vuepress/client.js
+++ blog/.vuepress/client.js
@@ -65,17 +65,10 @@ export default defineClientConfig({
     // });
   },
   setup() {
-    window.onload = function () {
-      console.log("111", window.wx);
-    };
-
     useScriptTag(
       "/sdk/jweixin-1.6.0.js",
       // on script tag loaded.
       async (el) => {
-        // do something
-        console.log("333", window.wx);
-
         const url = location.origin + location.pathname;
         const res = await fetchSign(url);
         console.log("res", res);
@@ -87,7 +80,7 @@ export default defineClientConfig({
             timestamp: res.data.timestamp, // 必填，生成签名的时间戳
             nonceStr: res.data.nonceStr, // 必填，生成签名的随机串
             signature: res.data.signature, // 必填，签名
-            jsApiList: [], // 必填，需要使用的JS接口列表
+            jsApiList: ["updateAppMessageShareData"], // 必填，需要使用的JS接口列表
           });
           wx.ready(function () {
             // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
