437de10b4126c8b087225f5a9c1d4efbb06e82ab
xinbao <fa@ijust.cc>
2025-08-24 16:27:37 +0800

    feat: move tailwind v3

--- mindtech-podcast/astro.config.mjs
+++ mindtech-podcast/astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config'
 import vue from '@astrojs/vue'
-import tailwind from '@astrojs/tailwind'
+
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  integrations: [vue(), tailwind()],
+  integrations: [vue()],
   output: 'static',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 })
